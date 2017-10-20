3ef8452f3575aba909dab773544ff666c60fb1bb
Eric Richter <richteer@lastprime.net>
2017-10-20 17:10:28 -0500

    added initial version of HalConsole agent

---
+++ halconsole.py
@@ -0,0 +1,27 @@
+from halibot import HalAgent, Message
+from .console import Console
+from threading import Thread
+
+# Implements overrides the .send() for wiring to Halibot core
+class NewConsole(Console):
+
+	def send(self, msg):
+		super().send(msg)
+		msg0 = Message(body=msg, author=self.agent.author)
+		self.agent.dispatch(msg0)
+
+class HalConsole(HalAgent):
+
+	def init(self):
+		self.con = NewConsole()
+		self.con.agent = self
+		self.author = self.config.get("author","user")
+		self.thread = Thread(target=self._loop)
+		self.thread.start()
+
+	def receive(self, msg):
+		self.con.chat.append("{}: {}".format(msg.author, msg.body))
+
+	def _loop(self):
+		self.con.run()
+		self._hal.shutdown()
